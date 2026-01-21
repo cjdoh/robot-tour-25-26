21fdcd23da800e0d33ff9dfe0dff04906b6f2566
cj <cjfrausto8153@gmail.com>
2026-01-21 00:35:12 -0800

    Add new primative and movement functions

--- ROB3_v1.ino
+++ ROB3_v1.ino
@@ -6,8 +6,8 @@
 
 // ---------------    Parameters   ---------------
 
-const double BLOCK_SIZE = 50.0;          // length of one grid square (in centimeters)
-const double ENCODER_PER_CENTIMETER = 0.0;          // encoder pulses per 1 cm
+const double BLOCK_SIZE = 50.0;          // Length of one grid square (in centimeters)
+const double ENCODER_PER_CENTIMETER = 0.0;          // Encoder pulses per 1 cm
 const double MOTOR_SPEED = 100.0;         // Base speed of both motors
 const double MOTOR_SPEED_MULTIPLIER = 1.13;         // In case one motor is slower than the other
 
@@ -43,11 +43,17 @@ Motor motorRight(BIN1, BIN2, PWMB, 1, STBY);
 // Encoders
 long encoderEnd; // Variable to hold the amount of encoder pulses to reach the target
 // Left Encoder
-boolean encoderLeftDirection;          // tracks the direction of the left motor (TRUE is foward)
-long encoderPulsesLeft;         // tracks the amount of encoder pulses in the left motor
+boolean encoderLeftDirection;          // Tracks the direction of the left motor (TRUE is foward)
+long encoderPulsesLeft;         // Tracks the amount of encoder pulses in the left motor
 // Right Encoder
-boolean encoderRightDirection;         // tracks the direction of the right motor (TRUE is foward)
-long encoderPulsesLeft;         // tracks the amount of encoder pulses in the left motor
+boolean encoderRightDirection;         // Tracks the direction of the right motor (TRUE is foward)
+long encoderPulsesLeft;         // Tracks the amount of encoder pulses in the left motor
+
+// ---------------       PID       ---------------
+
+
+// ~~~~~~~~~~~~~~~ TODO: Add PID variables ~~~~~~~~~~~~~~~
+
 
 // ---------------  Miscellaneous  ---------------
 
@@ -62,20 +68,159 @@ bool beginPath = false;         // If set to TRUE, the set movement primitives w
 
 unsigned long previousMillis = millis();          // Variable that holds the previous timestamp
 
+void fw(double blocks = 1.0);         // Instantiate primitives to allow for 
+void bw(double blocks = 1.0);         // a default parameter of 1 block
+
+// -----------------------------------------------
+
+
+
+// Main
+
 void setup(){
-  Serial.begin(9600);
+
+  // Start debug serial
+  Serial.begin(115200);
+  // Start compass serial
+  Serial1.begin(9600);
+
+  // Initialize start button pin as an input
+  pinMode(BUTTON_PIN, INPUT);
+
+  // Initialize encoders
+  encoderPulsesLeft, encoderPulsesRight = 0;
+  EncodersInit();
+  
+  // Initialize PID
+
+  // Initialize compass
+  JY901.attach(Serial1);
 
   
 }
 
 void loop(){
-  
+
+  // Read the state of the start button
+  buttonState = digitalRead(BUTTON_PIN); 
+
+  // Determine if the start button has been press (HIGH means the button is being pressed)
+  if (buttonState == HIGH && lastButtonState == LOW){
+
+    // Give time to take finger off of the button
+    delay(2000); 
+
+    // Begin the given path
+    beginPath = true;
+
+  }
+  // Save the start button state for the next loop
+  lastButtonState = buttonState;
+
+  if (beginPath){
+    Serial.print("##### RUNNING ######");
+
+    // Move into the first square
+    moveDistance(20.0);
+
+    // ---------------  Create Path Here  ---------------
+
+
+
+    // --------------------------------------------------
+
+    // Move dowel to the ending location
+    moveDistance(5.0);
+
+    // Stop running the path
+    beginPath = false;
+  }
 }
 
+// Primitives
+
+void fw(double blocks){
+  moveDistance(blocks * BLOCK_SIZE);
+}
+
+void bw(double blocks){
+  moveDistance(-blocks * BLOCK_SIZE);
+}
+
+void left(){
+  turnAngle(-90.0);
+}
+
+void right(){
+  turnAngle(90.0);
+}
+
+// Movement Functions
+
+void moveDistance(double distance){
+
+  // Set encoders
+  encoderEnd = abs(distance * ENCODER_PER_CENTIMETER);
+  encoderPulsesLeft = 0;
+  encoderPulsesRight = 0;
+
+  // Determine direction of target distance
+  int direction;
+  if (distance >= 0){
+    direction = 1; // Target is ahead
+  } else {
+    direction = -1; // Target is behind
+  }
+
+  // Tell motors to drive in the direction of the target distance
+  motorLeft.drive(MOTOR_SPEED * MOTOR_SPEED_MULTIPLIER * direction);
+  motorRight.drive(MOTOR_SPEED * direction);
+
+  while (abs(encoderPulsesLeft) < encoderEnd && abs(encoderPulsesRight) < encoderEnd){ // Wait for the encoders to count to the target pulse count
+    // Wait
+
+    // ~~~~~~~~~~~~~~~ TODO: Fix the PID / keep the robot moving straight without relying on the initial angle of the robot ~~~~~~~~~~~~~~~
+
+  }
+
+  // Stop moving after target distance is reached
+  hitBrakes();
+
+}
+
+void turnAngle(double degrees){
+
+  // Determine direction of target angle
+  int direction;
+  if (degrees >= 0){
+    direction = 1; // Turn to the right
+  } else {
+    direction = -1; // Turn to the left
+  }
+
+  // Tell motors to drive according to the direction of the angle
+  motorLeft.drive(MOTOR_SPEED * MOTOR_SPEED_MULTIPLIER * direction);
+  motorRight.drive(MOTOR_SPEED * -direction); // Moves the opposite direction in order to turn
+
+  // ~~~~~~~~~~~~~~~ TODO: Create a reliable way to turn a certain amount of degrees ~~~~~~~~~~~~~~~
+  
+  delay(2000); // Wait two seconds (temporary)
+
+  // Stop moving after reaching target angle
+  hitBrakes();
+}
+
+void hitBrakes(){
+
+  brake(motorLeft,motorRight);
+
+}
+
+// Encoder Functions
 
 void EncodersInit() {
-  encoderLeftDirection = true;                           // true -> Forward
-  encoderRightDirection = true;                           // true -> Forward
+  encoderLeftDirection = true;                           // TRUE -> Forward
+  encoderRightDirection = true;                           // TRUE -> Forward
 
   pinMode(ENCODER_LEFT_PIN_B,INPUT);
   pinMode(ENCODER_RIGHT_PIN_B,INPUT);
@@ -93,16 +238,16 @@ void EncoderLeftCounter() {
     // Check for change in direction
     if (leftStateB == LOW && encoderLeftDirection)
     {
-      encoderLeftDirection = false;                      // Backward
+      encoderLeftDirection = false; // Backward
     }
     else if (leftStateB == HIGH && !encoderLeftDirection)
     {
-      encoderLeftDirection = true;                       // Forward
+      encoderLeftDirection = true; // Forward
     }
   }
   encoderLeftLastState = leftStateA;
 
-  if(encoderLeftDirection)  encoderPulsesLeft++;                // NOTE: This is opposite of the right encoder since they are inverted
+  if(encoderLeftDirection)  encoderPulsesLeft++; // NOTE: This is opposite of the right encoder since they are inverted
   else  encoderPulsesLeft--;
 }
 
@@ -115,22 +260,15 @@ void EncoderRightCounter() {
     // Check for change in direction
     if (rightStateB == LOW && encoderRightDirection)
     {
-      encoderRightDirection = false;                      // Backward
+      encoderRightDirection = false; // Backward
     }
     else if (rightStateB == HIGH && !encoderRightDirection)
     {
-      encoderRightDirection = true;                       // Forward
+      encoderRightDirection = true; // Forward
     }
   }
   encoderRightLastState = rightStateA;
 
-  if(encoderRightDirection)  encoderPulsesRight++;                // NOTE: This is opposite of the right encoder since they are inverted
+  if(encoderRightDirection)  encoderPulsesRight++;
   else  encoderPulsesRight--;
-}
-
-
-void hit_brakes(){
-
-  brake(motorLeft,motorRight);
-
 }
